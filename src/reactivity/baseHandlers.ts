eb3950b5377bd9cc9659a4f98c9c70296cbfed2c
xiao18100171354 <xiao_97911@163.com>
2022-10-04 17:15:37 +0800

    优化set和get

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,50 @@
+import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
+
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+// ! 封装 getter 函数
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+
+
+    const res = Reflect.get(target, key);
+
+    if (!isReadonly) {
+      // 如果不是 readonly
+      // ! 依赖收集
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+// ! 封装 setter 函数
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // ! 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key:${key} set 失败 因为targer是readonly`, target);
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,32 +1,36 @@
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 import { track, trigger } from "./effect";
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      // {foo: 1}
-      const res = Reflect.get(target, key);
-      // 依赖收集
-      track(target, key);
-      return res;
-    },
+  return createActiveObject(raw, mutableHandlers);
+  // return new Proxy(raw, {
+  //   get(target, key) {
+  //     // {foo: 1}
+  //     const res = Reflect.get(target, key);
+  //     // 依赖收集
+  //     track(target, key);
+  //     return res;
+  //   },
 
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // todo 触发依赖
-      trigger(target, key);
-      return res;
-    },
-  });
+  //   set(target, key, value) {
+  //     const res = Reflect.set(target, key, value);
+  //     // todo 触发依赖
+  //     trigger(target, key);
+  //     return res;
+  //   },
+  // });
 }
 
 export function readonly(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      return true;
-    },
-  });
+  return createActiveObject(raw, readonlyHandlers);
+  // return new Proxy(raw, {
+  //   get: createGetter(true),
+  //   set(target, key, value) {
+  //     return true;
+  //   },
+  // });
+}
+
+function createActiveObject(raw: any, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
