f34bd5bc230a8ad897069effe2012bb83fceffb7
xiao18100171354 <xiao_97911@163.com>
2022-10-18 22:31:15 +0800

    通过实现 setupSateus 的思想，实现 props 功能点2：在 render 函数中，可以通过 this 来访问 props 中的属性

--- src/runtime-core/componentPublicInstace.ts
+++ src/runtime-core/componentPublicInstace.ts
@@ -5,9 +5,22 @@ const publicPropertiesMap = {
 export const PublicInstanceProxyHandlers = {
   get({ _: instance }, key) {
     // setupStatus
-    const { setupStatus } = instance;
-    if (key in setupStatus) {
+    const { setupStatus, props } = instance;
+
+    const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key);
+
+    // if (key in setupStatus) {
+    //   return setupStatus[key];
+    // }
+    if (hasOwn(setupStatus, key)) {
       return setupStatus[key];
+    }
+
+    // if (key in props) {
+    //   return props[key];
+    // }
+    if (hasOwn(props, key)) {
+      return props[key];
     }
 
     // if (key === "$el") {
